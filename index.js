651b9d161c80dade999d54f4e4b1f2513596ebdd
faustaciku <faustaciku@gmail.com>
2022-06-25 12:20:10 +0100

    Fixed a bug
    I had to press twice on a description button to see the content. I fixed the if statement and problem was solved.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function displayText(number) {
   let paragraph = document.getElementById("p" + number);
   let card = document.getElementById("card" + number);
-  if (paragraph.style.display === "none") {
-    paragraph.style.display = "inline";
+  if (paragraph.style.display != "block") {
+    paragraph.style.display = "block";
     card.style.height = "auto";
   } else {
     paragraph.style.display = "none";
